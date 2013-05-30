6e33d6d576b6528cf12d809e73fe685a95811b08
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-05-30 01:09:39 -0400

    added

--- application/views/portfolio.php
+++ application/views/portfolio.php
@@ -1,14 +1,14 @@
 <div id="decor"><p>Porfolio</p></div>
 <div id="main">
 
-	<a class="placeholder fancybox" href="./assets/img/img2-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img2-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img13-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img13-small.jpg" /></a>
-	<a class="placeholder fancybox" href="./assets/img/img6-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img6-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img10-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img10-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img12-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img12-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img5-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img5-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img13-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img13-small.jpg" /></a>
-	<a class="placeholder fancybox"  href="./assets/img/img11-large.jpg" title="Easter colors"><img alt="Easter Colors makeup" src="./assets/img/img11-small.jpg" /></a>
+	<a class="placeholder fancybox" href="./assets/img/img2-large.jpg" title="Easter"><img alt="Easter Colors makeup" src="./assets/img/img2-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img13-large.jpg" title="The Alchemyst Full face"><img alt="Easter Colors makeup" src="./assets/img/img13-small.jpg" /></a>
+	<a class="placeholder fancybox" href="./assets/img/img6-large.jpg" title="Pride"><img alt="Easter Colors makeup" src="./assets/img/img6-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img10-large.jpg" title="Into the Darkness"><img alt="Easter Colors makeup" src="./assets/img/img10-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img12-large.jpg" title="St. Pattys"><img alt="Easter Colors makeup" src="./assets/img/img12-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img5-large.jpg" title="Forest"><img alt="Easter Colors makeup" src="./assets/img/img5-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img14-large.jpg" title="Simple"><img alt="Easter Colors makeup" src="./assets/img/img14-small.jpg" /></a>
+	<a class="placeholder fancybox"  href="./assets/img/img11-large.jpg" title="Color Party"><img alt="Easter Colors makeup" src="./assets/img/img11-small.jpg" /></a>
 	<a class="placeholder fancybox"  href="./assets/img/img7-large.jpg" title="Smokey Gold with glitter"><img alt="Easter Colors makeup" src="./assets/img/img7-small.jpg" /></a>
 	<a class="placeholder fancybox"  href="./assets/img/img8-large.jpg" title="The Alchemyst"><img alt="Easter Colors makeup" src="./assets/img/img8-small.jpg" /></a>
 
